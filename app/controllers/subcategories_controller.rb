e7cc78d863a68707d3e2d99bd6008b4d4e0e58e2
John <ristaujo@gmail.com>
2017-10-19 13:29:25 -0400

    Links added to subcategories

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,11 +1,13 @@
 class SubcategoriesController < ApplicationController 
   
   def show
+    @listings = Listing.where(subcategory_id: params[:id])
   end
 
   def find_by_category
     category = Category.find(params[:category_id])
     subcategories = category.subcategories.find_all
     render json: { subcategories: subcategories }
+  end
 
 end
